3a8937e666876182064a71b7f4a79a5020171786
eijaz325@gmail.com <eijaz325@gmail.com>
2018-11-26 23:39:50 +0530

    Single Page Application

--- react/confusion/src/components/MainComponent.js
+++ react/confusion/src/components/MainComponent.js
@@ -9,6 +9,7 @@ import { COMMENTS } from "../shared/comments";
 import { PROMOTIONS } from "../shared/promotions";
 import { LEADERS } from "../shared/leaders";
 import { Switch, Route, Redirect } from "react-router-dom";
+import DishDetail from "./DishdetailComponent";
 
 class MainComponent extends Component {
   constructor(props) {
@@ -32,6 +33,22 @@ class MainComponent extends Component {
       );
     };
 
+    //Route will send match, location and history
+    const DishWithId = ({ match }) => {
+      return (
+        <DishDetail
+          dish={
+            this.state.dishes.filter(
+              dish => dish.id === parseInt(match.params.dishId, 10)
+            )[0]
+          }
+          comments={this.state.comments.filter(
+            comment => comment.dishId === parseInt(match.params.dishId, 10)
+          )}
+        />
+      );
+    };
+
     return (
       <div>
         <Header />
@@ -43,6 +60,7 @@ class MainComponent extends Component {
             component={() => <Menu dishes={this.state.dishes} />}
           />
           <Route exact path="/contactus" component={Contact} />
+          <Route path="/menu/:dishId" component={DishWithId} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
--- react/confusion/src/shared/leaders.js
+++ react/confusion/src/shared/leaders.js
@@ -2,7 +2,7 @@ export const LEADERS = [
   {
     id: 0,
     name: "Peter Pan",
-    image: "/assets/images/alberto.png",
+    image: "/assets/images/abdul.jpg",
     designation: "Chief Epicurious Officer",
     abbr: "CEO",
     featured: false,
@@ -12,7 +12,7 @@ export const LEADERS = [
   {
     id: 1,
     name: "Dhanasekaran Witherspoon",
-    image: "/assets/images/alberto.png",
+    image: "/assets/images/abdul.jpg",
     designation: "Chief Food Officer",
     abbr: "CFO",
     featured: false,
@@ -22,7 +22,7 @@ export const LEADERS = [
   {
     id: 2,
     name: "Agumbe Tang",
-    image: "/assets/images/alberto.png",
+    image: "/assets/images/abdul.jpg",
     designation: "Chief Taste Officer",
     abbr: "CTO",
     featured: false,
@@ -31,8 +31,8 @@ export const LEADERS = [
   },
   {
     id: 3,
-    name: "Alberto Somayya",
-    image: "/assets/images/alberto.png",
+    name: "Mohammed Abdul Khaliq",
+    image: "/assets/images/abdul.jpg",
     designation: "Executive Chef",
     abbr: "EC",
     featured: true,
